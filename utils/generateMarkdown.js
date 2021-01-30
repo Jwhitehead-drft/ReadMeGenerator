0feec3b6eb15b70cf833e24236af2a09c920c481
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2021-01-29 19:03:48 -0500

    Fixed variables in generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,52 @@
 // TODO: Create a const that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) {
-
-}
+const renderLicenseBadge = (license) => {
+    if (license !== "None") {
+      return `![github](https://img.shields.io/badge/license-${license}-yellow.svg)`;
+    }  
+    return '';
+  };
 
 // TODO: Create a const that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) {
-
-}
+const renderLicenseLink = (license) => {
+  if (license !== "None") {
+    return `[Click here to view license](${license})`;
+  } else { 
+    return '';
+  }
+};
 
 // TODO: Create a const that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = (license) {
-
-}
+const renderLicenseSection = (license) => {
+  if (license !== "None") {
+    return `This project is licensed under ${license}`;
+  } else {
+    return '';
+  }
+};
 
 // TODO: Create a const to generate markdown for README
-const generateMarkdown = (data) {
-  return `# ${data.title},
+const generateMarkdown = function(data) {
+  return `# ${data.project},
 
 
-##Title ${data.title}
+##Title
+
+${data.project},
+
+###Description
+
+${data.description},
+
+####Dependencies
+
+####Email
+
+If you have questions regarding this project, you can contact me at ${data.email},
+
 `;
-}
+};
 
 module.exports = generateMarkdown;
